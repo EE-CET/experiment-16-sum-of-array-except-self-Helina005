e8009fbb8707ba692fba655ef322c0344afb2aa0
Helina Alex <helinatalex@gmail.com>
2026-04-03 00:10:04 +0530

    sum of array

--- src/SumExceptSelf.java
+++ src/SumExceptSelf.java
@@ -1,8 +1,37 @@
-public class SumExceptSelf {
    
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Calculate sum of all elements (Total Sum)
-        // TODO: For each element, print (Total Sum - element)
-    
+        import java.util.Scanner;
+
+public class SumExceptSelf {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read the size of the array
+        if (!sc.hasNextInt()) return;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        long totalSum = 0;
+
+        // Read array elements and calculate total sum
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            totalSum += arr[i];
+        }
+
+        // Calculate and print the result
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            long resultValue = totalSum - arr[i];
+            output.append(resultValue);
+            
+            // Add space between elements, but not after the last one
+            if (i < n - 1) {
+                output.append(" ");
+            }
+        }
+
+        System.out.println(output.toString());
+        sc.close();
+    }
+
 }
